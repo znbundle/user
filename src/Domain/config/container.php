2e2e231f1d10bdf23ddc33709980b6dc9b56028c
Vitaliy <theyamshikov@yandex.ru>
2020-12-17 13:49:46 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'ZnBundle\User\Domain\Interfaces\Services\AuthServiceInterface' => 'ZnBundle\User\Domain\Services\AuthService2',
     'ZnBundle\User\Domain\Interfaces\Services\IdentityServiceInterface' => 'ZnBundle\User\Domain\Services\IdentityService',
     'ZnBundle\User\Domain\Interfaces\Services\CredentialServiceInterface' => 'ZnBundle\User\Domain\Services\CredentialService',
     'ZnBundle\User\Domain\Interfaces\Repositories\IdentityRepositoryInterface' => 'ZnBundle\User\Domain\Repositories\Eloquent\IdentityRepository',
