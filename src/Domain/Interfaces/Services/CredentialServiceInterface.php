3c169aa73b0a87c3e14c3e37dfe4cba2b196965f
Vitaliy <theyamshikov@yandex.ru>
2021-05-13 23:44:41 +0600

    upd

--- src/Domain/Interfaces/Services/CredentialServiceInterface.php
+++ src/Domain/Interfaces/Services/CredentialServiceInterface.php
@@ -3,12 +3,23 @@
 namespace ZnBundle\User\Domain\Interfaces\Services;
 
 use ZnBundle\User\Domain\Entities\CredentialEntity;
+use ZnCore\Base\Exceptions\NotFoundException;
 
 interface CredentialServiceInterface
 {
-
+    /**
+     * @param int $identityId
+     * @param string $type
+     * @return CredentialEntity
+     * @throws NotFoundException
+     */
     public function oneByIdentityIdAndType(int $identityId, string $type): CredentialEntity;
 
+    /**
+     * @param string $credential
+     * @return CredentialEntity
+     * @throws NotFoundException
+     */
     public function oneByCredentialValue(string $credential): CredentialEntity;
 }
 
