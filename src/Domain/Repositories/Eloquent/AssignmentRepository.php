0c26a7dc4827cfaa58120139c11fb8b71dfed7bc
Vitaliy <theyamshikov@yandex.ru>
2021-05-18 22:47:07 +0600

    upd

--- src/Domain/Repositories/Eloquent/AssignmentRepository.php
+++ src/Domain/Repositories/Eloquent/AssignmentRepository.php
@@ -2,7 +2,9 @@
 
 namespace ZnBundle\User\Domain\Repositories\Eloquent;
 
+use Illuminate\Support\Collection;
 use ZnBundle\User\Domain\Entities\AssignmentEntity;
+use ZnCore\Domain\Libs\Query;
 use ZnLib\Db\Base\BaseEloquentCrudRepository;
 
 class AssignmentRepository extends BaseEloquentCrudRepository /*implements AssignmentRepositoryInterface*/
@@ -13,5 +15,11 @@ class AssignmentRepository extends BaseEloquentCrudRepository /*implements Assig
     public function getEntityClass(): string
     {
         return AssignmentEntity::class;
+    }
+
+    public function allByIdentityId(int $identityId): Collection {
+        $query = new Query();
+        $query->where('user_id', $identityId);
+        return $this->all($query);
     }
 }
