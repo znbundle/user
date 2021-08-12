0ef5762ba7e1f2572f61d0d3582d34aab3190098
Vitaliy <theyamshikov@yandex.ru>
2021-08-12 20:31:40 +0600

    upd

---
+++ src/Domain/Interfaces/Services/RegistrationServiceInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ZnBundle\User\Domain\Interfaces\Services;
+
+use ZnBundle\User\Domain\Exceptions\UnauthorizedException;
+use ZnBundle\User\Domain\Forms\AuthForm;
+use ZnBundle\User\Domain\Interfaces\Entities\IdentityEntityInterface;
+
+interface RegistrationServiceInterface
+{
+
+    /**
+     * @return IdentityEntityInterface
+     * @throws UnauthorizedException
+     */
+    public function getIdentity(): IdentityEntityInterface;
+    public function setIdentity(IdentityEntityInterface $identityEntity);
+    //public function authenticationByForm(LoginForm $loginForm);
+    public function authenticationByToken(string $token, string $authenticatorClassName = null);
+    public function tokenByForm(AuthForm $form);
+
+}
---
+++ src/Rpc/Controllers/RegistrationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ZnBundle\User\Rpc\Controllers;
+
+use ZnBundle\User\Domain\Entities\TokenValueEntity;
+use ZnBundle\User\Domain\Forms\AuthForm;
+use ZnBundle\User\Domain\Interfaces\Services\AuthServiceInterface;
+use ZnCore\Domain\Helpers\EntityHelper;
+use ZnLib\Rpc\Domain\Entities\RpcRequestEntity;
+use ZnLib\Rpc\Domain\Entities\RpcResponseEntity;
+
+class RegistrationController
+{
+
+    private $service;
+
+    public function __construct(AuthServiceInterface $authService)
+    {
+        $this->service = $authService;
+    }
+
+    public function getTokenByPassword(RpcRequestEntity $requestEntity): RpcResponseEntity
+    {
+        $form = new AuthForm();
+        EntityHelper::setAttributes($form, $requestEntity->getParams());
+        /** @var TokenValueEntity $tokenEntity */
+        $tokenEntity = $this->service->tokenByForm($form);
+        $result = [
+            'token' => $tokenEntity->getTokenString()
+        ];
+        $response = new RpcResponseEntity();
+        $response->setResult($result);
+        return $response;
+    }
+}
