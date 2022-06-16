8a0a6d3a69b703d613f199760aaf68eaf322720e
Vitaliy <theyamshikov@yandex.ru>
2022-06-16 13:00:45 +0600

    upd

--- src/Symfony4/Web/Subscribers/WebAuthSubscriber.php
+++ src/Symfony4/Web/Subscribers/WebAuthSubscriber.php
@@ -4,17 +4,15 @@ namespace ZnBundle\User\Symfony4\Web\Subscribers;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use ZnBundle\User\Domain\Enums\WebCookieEnum;
 use ZnBundle\User\Domain\Interfaces\Services\AuthServiceInterface;
 use ZnBundle\User\Domain\Interfaces\Services\IdentityServiceInterface;
-use ZnCore\Base\Exceptions\InvalidConfigException;
-use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
 use ZnCore\Base\Libs\DotEnv\DotEnv;
 use ZnCore\Domain\Helpers\EntityHelper;
 use ZnLib\Web\Symfony4\MicroApp\Enums\ControllerEventEnum;
-use ZnLib\Web\Symfony4\MicroApp\Events\ControllerEvent;
+//use ZnLib\Web\Symfony4\MicroApp\Events\ControllerEvent;
 use ZnLib\Web\Symfony4\MicroApp\Libs\CookieValue;
-use ZnUser\Rbac\Domain\Interfaces\Services\ManagerServiceInterface;
 
 class WebAuthSubscriber implements EventSubscriberInterface
 {
@@ -44,16 +42,17 @@ class WebAuthSubscriber implements EventSubscriberInterface
     public function onBeforeRunAction(ControllerEvent $event)
     {
         $identityArray = $this->session->get('user.identity');
-        if(!$identityArray) {
+        if (!$identityArray) {
             $identityIdCookie = $event->getRequest()->cookies->get(WebCookieEnum::IDENTITY_ID);
-            if($identityIdCookie) {
+            if ($identityIdCookie) {
                 try {
                     $cookieValue = new CookieValue(DotEnv::get('CSRF_TOKEN_ID'));
                     $identityId = $cookieValue->decode($identityIdCookie);
                     $identity = $this->identityService->oneById($identityId);
                     $this->authService->setIdentity($identity);
                     $this->session->set('user.identity', EntityHelper::toArray($identity));
-                } catch (\DomainException $e) {}
+                } catch (\DomainException $e) {
+                }
             }
         }
     }
